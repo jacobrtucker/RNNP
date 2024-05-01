83683421f609cf2dbe92ffc73d8e110adab95813
Jacob Tucker <jrtucker@princeton.edu>
2024-04-30 23:06:58 -0400

    making more progress

--- RNNP.py
+++ RNNP.py
@@ -1,42 +1,88 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Concatenate, Dropout
+import tensorflow_probability as tfp
+import os
+import datetime
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def build_model(num_demographic_features, num_days, num_day_of_week, num_week_of_year, num_prev_proportions):
-    # Inputs
-    demographics_input = Input(shape=(num_demographic_features,), name="demographics")
-    day_input = Input(shape=(1,), name="day_of_survey")
-    day_of_week_input = Input(shape=(1,), name="day_of_week")
-    week_of_year_input = Input(shape=(1,), name="week_of_year")
-    prev_proportions_input = Input(shape=(num_prev_proportions,), name="previous_day_proportions")
+class RNNP:
+    model = None
+    num_outcomes = None
+    outcome_dims = None
+    posp = {}
 
-    # Embeddings for categorical inputs
-    day_embed = Embedding(input_dim=num_days, output_dim=4)(day_input)
-    day_of_week_embed = Embedding(input_dim=num_day_of_week, output_dim=2)(day_of_week_input)
-    week_of_year_embed = Embedding(input_dim=num_week_of_year, output_dim=2)(week_of_year_input)
+    def __init__(self, demo_feature_names, num_demographic_features, num_days, num_outcomes, outcome_dims):
+        # Inputs
+        # demographics_input = Input(shape=(num_demographic_features,), name="demographics")
+        day_input = Input(shape=(1,), name="day_of_survey")
+        day_of_week_input = Input(shape=(1,), name="day_of_week")
+        week_of_year_input = Input(shape=(1,), name="week_of_year")
 
-    # Flatten embeddings
-    day_embed_flat = tf.keras.layers.Flatten()(day_embed)
-    day_of_week_flat = tf.keras.layers.Flatten()(day_of_week_embed)
-    week_of_year_flat = tf.keras.layers.Flatten()(week_of_year_embed)
+        inputs = [day_input, day_of_week_input, week_of_year_input]
 
-    # Concatenate all features
-    concatenated = Concatenate()([demographics_input, day_embed_flat, day_of_week_flat, week_of_year_flat, prev_proportions_input])
+        # Embeddings for categorical inputs
+        day_embed = Embedding(input_dim=num_days, output_dim=4)(day_input)
+        day_of_week_embed = Embedding(input_dim=7, output_dim=2)(day_of_week_input)
+        week_of_year_embed = Embedding(input_dim=52, output_dim=2)(week_of_year_input)
 
-    # Recurrent layer to capture temporal dependencies
-    lstm_layer = LSTM(50)(tf.expand_dims(concatenated, axis=1))
+        # Flatten embeddings
+        day_embed_flat = tf.keras.layers.Flatten()(day_embed)
+        day_of_week_flat = tf.keras.layers.Flatten()(day_of_week_embed)
+        week_of_year_flat = tf.keras.layers.Flatten()(week_of_year_embed)
 
-    # Fully connected layers
-    dense1 = Dense(100, activation='relu')(lstm_layer)
-    dropout = Dropout(0.5)(dense1)
-    output = Dense(num_prev_proportions, activation='softmax')(dropout)  # assuming output is a probability distribution
+        features = [day_embed_flat, day_of_week_flat, week_of_year_flat]
+        for dem in demo_feature_names:
+            num_dims = round(num_demographic_features[dem] ** 0.25)
+            if num_dims < 1:
+                num_dims = 1
+            inputs.append(Input(shape=(1,), name=dem))
+            features.append(tf.keras.layers.Flatten()(Embedding(input_dim=num_demographic_features[dem],output_dim=num_dims)(inputs[-1])))
 
-    # Build model
-    model = Model(inputs=[demographics_input, day_input, day_of_week_input, week_of_year_input, prev_proportions_input], outputs=output)
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        # Concatenate all features
+        concatenated = Concatenate()(features)
+
+        # Recurrent layer to capture temporal dependencies
+        lstm_layer = LSTM(100)(tf.expand_dims(concatenated, axis=1))
+
+        # Fully connected layers
+        dense1 = Dense(100, activation='relu')(lstm_layer)
+        dropout = Dropout(0.5)(dense1)
+        dense2 = Dense(100, activation='relu')(dropout)
+        dropout2 = Dropout(0.5)(dense2)
+        output = Dense(sum(outcome_dims), activation='sigmoid')(dropout2)  # assuming output is a probability distribution
+
+        # Build model
+        model = Model(inputs=inputs, outputs=output)
+        model.compile(optimizer='adam', loss=self.negDirLL, metrics=['accuracy'])
+        
+        self.model = model
+        self.num_outcomes = num_outcomes
+        self.outcome_dims = outcome_dims
+
+        for od in range(num_outcomes):
+            self.posp[od] = []
+            for i in range(10000):
+                self.posp[od].append([tfp.distributions.Uniform(low=0.0, high=1.0) for j in range(outcome_dims[od])])
     
-    return model
+    def fit(self, X_train, y_train, epochs=10, batch_size=32):
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+    
+    def negDirLL(self, y_true, y_pred):
+        total_counts = y_true[:-self.num_outcomes]
+        nll = float(0)
+        start_index = 0
+        for i in range(self.num_outcomes):
+            end_index = start_index + self.outcome_dims[i]
+            y = y_true[start_index:end_index]
+            yp = y_pred[start_index:end_index]
+            yp = tf.softmax(yp)
+            for posp in self.posp[i]:
+                nll += -tfp.distributions.Dirichlet(posp).log_prob(yp) - tfp.distributions.Multinomial(total_counts[i], probs=yp).log_prob(y)
+            start_index = end_index
+        return nll
 
-# Example of dimensions, assuming some typical values
-model = build_model(num_demographic_features=10, num_days=365, num_day_of_week=7, num_week_of_year=52, num_prev_proportions=5)
-model.summary()
+    def save(self, path=None):
+        if path is None:
+            path = os.path.join(dir_path, 'modelcache/RNNP-' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
+        self.model.save(path)
--- test-run.py
+++ test-run.py
@@ -1,12 +1,44 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(os.path.join(dir_path, '../rutracker'))
+from qry.qry import qry
+import pdb
 
-# Assuming 'df' is your DataFrame
+df = qry(weight_type=None, datacachefile=True, datacachedate="2024-04-27")
+pdb.set_trace()
+
+DEMO_VARS = ['gender', 'age_range', 'education', 'area']
+OUTCOME_VARS = ['ukraine_approval', 'statementagree_smo_successful']
+
+# Subset df to only include values of survey_date where there is at least one non-null value for the outcome variables
+nonnulldates = df.survey_date.loc[df[OUTCOME_VARS].notnull().any(axis=1)].unique()
+df = df.loc[df.survey_date.isin(nonnulldates)]
+
+vocab = {}
+for var in DEMO_VARS:
+    ctr = 1
+    vocab[var] = {}
+    for val in df[var].unique():
+        vocab[var][val] = ctr
+        ctr += 1
+    df[var] = df[var].map(vocab[var])
+
+start_date = df.survey_date.min()
+df['day_of_survey'] = (df['survey_date'] - start_date).dt.days + 1
+df['day_of_week'] = df['survey_date'].dt.dayofweek
+df['week_of_year'] = df['survey_date'].dt.isocalendar().week
+
+DAY_VARS = ['day_of_survey', 'day_of_week', 'week_of_year']
+df = df[DEMO_VARS + DAY_VARS + OUTCOME_VARS]
+
 
 # One-hot encode categorical demographic data
 encoder = OneHotEncoder(sparse=False)
-demographic_features = encoder.fit_transform(df[['gender', 'age', 'education', 'area']])
+demographic_features = encoder.fit_transform(df[['gender', 'age_range', 'education', 'area']])
 demographic_feature_names = encoder.get_feature_names(['gender', 'age', 'education', 'area'])
 demographics_df = pd.DataFrame(demographic_features, columns=demographic_feature_names, index=df.index)
 
